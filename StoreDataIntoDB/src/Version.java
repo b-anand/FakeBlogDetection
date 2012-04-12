09d9f41ee3bbd469bc24a43606a9a88a4aaf6bf9
Anand <anand.bheemaraju@gmail.com>
2012-04-11 20:50:56 -0400

    Preprocess data from feeds and store them into Mysql Database.

---
+++ StoreDataIntoDB/src/Version.java
@@ -0,0 +1,86 @@
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Version {
+
+	private static String dataPath = "Z:\\FakeBlogDetection\\CompressedDataset\\blogDataSet\\";
+
+	private static void readDataFromFiles(String path) {
+		File folder = new File(path);
+		for (File f : folder.listFiles()) {
+			try {
+				DocumentBuilderFactory dbFactory = DocumentBuilderFactory
+						.newInstance();
+				DocumentBuilder dBuilder;
+				dBuilder = dbFactory.newDocumentBuilder();
+				Document doc;
+				doc = dBuilder.parse(f);
+				doc.getDocumentElement().normalize();
+				NodeList nl = doc.getDocumentElement().getElementsByTagName(
+						"description");
+				String text = "";
+				for (int i = 0; i < nl.getLength(); i++) {
+					text += nl.item(i).getTextContent();
+				}
+				Pattern pattern = Pattern.compile("\\p{Alpha}+");
+				Matcher matcher = pattern.matcher(text);
+				String newText = "";
+				while (matcher.find()) {
+					String currentText = matcher.group();
+					if (currentText.length() > 2
+							&& !(currentText.toUpperCase().equals(currentText)))
+						newText += currentText + " ";
+				}
+				int id = new Integer(f.getName().split(".xml")[0]).intValue();
+				addData(id, newText);
+				System.out.println(id);
+			} catch (SAXException | IOException | ParserConfigurationException e) {
+				// e.printStackTrace();
+			}
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+		readDataFromFiles(dataPath);
+	}
+
+	private static void addData(int id, String text) {
+		Connection con = null;
+
+		String url = "jdbc:mysql://localhost:3306/blogdata";
+		String user = "root";
+		String password = "nlp";
+
+		try {
+			con = DriverManager.getConnection(url, user, password);
+			String query = "INSERT INTO blogdata.data (id, text) VALUES (?, ?)";
+			PreparedStatement st = con.prepareStatement(query);
+			st.setInt(1, id);
+			st.setString(2, text);
+			st.execute();
+			con.close();
+		} catch (SQLException ex) {
+			Logger lgr = Logger.getLogger(Version.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+		}
+	}
+}
